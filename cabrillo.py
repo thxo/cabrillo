086de8e6b32ccdf64ce7a665bd6c5d9270742d78
Howard Xiao <thx@uw.edu>
2018-12-15 11:31:17 -0800

    Add categories

---
+++ cabrillo.py
@@ -0,0 +1,16 @@
+# Based on the Cabrillo specification available at
+# https://wwrof.org/cabrillo/cabrillo-specification-v3/
+# All values below should remain upper case.
+CATEGORY_ASSISTED = ['ASSISTED', 'NON-ASSISTED']
+CATEGORY_BAND = ['ALL', '160M', '80M', '40M', '20M', '15M', '10M', '6M', '4M',
+                 '2M', '222', '432', '902', '1.2G', '2.3G', '3.4G', '5.7G',
+                 '10G', '24G', '47G', '75G', '123G', '134G', '241G', 'LIGHT',
+                 'VHF-3-BAND', 'VHF-FM-ONLY']
+CATEGORY_MODE = ['SSB', 'CW', 'RTTY', 'FM', 'MIXED']
+CATEGORY_OPERATOR = ['SINGLE-OP', 'MULTI-OP', 'CHECKLOG']
+CATEGORY_POWER = ['HIGH', 'LOW', 'QRP']
+CATEGORY_STATION = ['FIXED', 'MOBILE', 'PORTABLE', 'ROVER', 'ROVER-LIMITED',
+                    'ROVER-UNLIMITED', 'EXPEDITION', 'HQ', 'SCHOOL']
+CATEGORY_TIME = ['6-HOURS', '12-HOURS', '24-HOURS']
+CATEGORY_TRANSMITTER = ['ONE', 'TWO', 'LIMITED', 'UNLIMITED', 'SWL']
+CATEGORY_OVERLAY = ['CLASSIC', 'ROOKIE', 'TB-WIRES', 'NOVICE-TECH', 'OVER-50']
