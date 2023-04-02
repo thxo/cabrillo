fbfc047c42f877ff139de367b359de0af1e20441
thxo <thx@uw.edu>
2023-04-02 01:40:03 -0700

    Cut v0.1.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="cabrillo",
-    version="0.1.0",
+    version="0.1.1",
     author="thxo",
     author_email="thx@uw.edu",
     description="A Python library to parse Cabrillo-format amateur radio "
