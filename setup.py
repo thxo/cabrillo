0f5021ceb2fd198c74d6caa81ca180e287a9c56b
thxo <21995564+thxo@users.noreply.github.com>
2024-10-05 18:43:20 +0000

    Cut v0.2.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="cabrillo",
-    version="0.1.2",
+    version="0.2.0",
     author="thxo",
     author_email="thxo@riseup.net",
     description="A Python library to parse Cabrillo-format amateur radio "
